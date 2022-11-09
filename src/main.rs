97fef61ddcf8e732c0481e5a0bd528b1446d5c28
toransdev <alessandro.torrini87@gmail.com>
2022-11-09 20:01:21 +0100

    remove comments in main function

--- src/main.rs
+++ src/main.rs
@@ -48,27 +48,4 @@ fn main() {
         }
         None => println!("{}", tile_grid),
     }
-
-    // println!("{}", TileGrid::new(10, 10).randomize());
-    // println!(
-    //     "{}",
-    //     TileGrid::new(5, 5).randomize().reflect_v().reflect_h()
-    // );
-    //
-    // println!("{}", Tile::from_bits(11));
-    // println!("{}", Tile::from_bits(11).transpose());
-    // Tile::new_random().save_svg("tile.svg", None);
-    // TileGrid::new(5, 5)
-    //     .randomize()
-    //     .reflect_v()
-    //     .reflect_h()
-    //     .save_svg("tile_grid.svg", None);
-
-    // Test
-    // let mut t = tile_grid::TileGrid::new(1, 2);
-    // t.randomize_mut(); // allowed
-    // t.randomize_mut().randomize_mut(); // allowed
-    // t.randomize(); // allowed
-    // t.randomize(); // allowed
-    // t.randomize().randomize(); // not allowed
 }
